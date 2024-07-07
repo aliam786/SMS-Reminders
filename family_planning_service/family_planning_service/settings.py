6a988bee483e56a949392c12c15daa1993e5b57f
Ali Mobeen <aam278@cornell.edu>
2024-07-07 23:20:11 +0000

    security settings before sleeping

--- family_planning_service/family_planning_service/settings.py
+++ family_planning_service/family_planning_service/settings.py
@@ -32,6 +32,13 @@ SECRET_KEY = SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+
+
 
 ALLOWED_HOSTS = ['alimobeen.pythonanywhere.com']
 
